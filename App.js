96ee83cbeb6c80111000d7d0ecf46127fdf46665
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-28 17:32:06 -0400

    Text input

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View, Platform} from 'react-native';
+import {StyleSheet, Text, View, Platform, TextInput} from 'react-native';
 
 const App = () => {
     return (
         <View style={styles.container}>
             <Text style={[styles.largeText, styles.textStyle]}>
-              San Francisco
+                San Francisco
             </Text>
             <Text style={[styles.smallText, styles.textStyle]}>
-              Light Cloud
+                Light Cloud
             </Text>
-          <Text style={[styles.largeText, styles.textStyle]}>
-            24º
-          </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>
+                24º
+            </Text>
+            <TextInput
+                autoCorrect={false}
+                placeholder="Search Any City"
+                placeholderTextColor="white"
+                style={styles.textInput}
+                clearButtonMode="always"
+            />
         </View>
     )
 };
@@ -33,6 +40,16 @@ const styles = StyleSheet.create({
     },
     smallText: {
         fontSize: 18
+    },
+    textInput: {
+      backgroundColor: "#666",
+      color: "white",
+      height: 40,
+      width: 300,
+      marginTop: 20,
+      marginHorizontal: 20,
+      paddingHorizontal: 10,
+      alignSelf: "center"
     }
 });
 
