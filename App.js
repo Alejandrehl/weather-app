9efa3cfac1e5a0739e8b7dfe6fbd7a0a7e0d3bb8
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-28 17:14:01 -0400

    Read me and first text

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Platform} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              San Francisco
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            24º
+          </Text>
+        </View>
+    )
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textStyle: {
+        textAlign: "center",
+        fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+    },
+    largeText: {
+        fontSize: 24
+    },
+    smallText: {
+        fontSize: 18
+    }
 });
+
+export default App;
+
